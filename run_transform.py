f7dcac629ac35d0db392856876503e1e979b73f3
the4thdoctor <4thdoctor.gallifrey@gmail.com>
2013-08-09 22:36:36 +0100

    completed the connection class. added the my_data_def class. dealing with sqlalchemy odd behaviour. definitely I don't like this approach; It's useful to get the database's metadata but extremely dangerous when abstracting the queries; the performance tuner's worst nightmare.

--- run_transform.py
+++ run_transform.py
@@ -1,2 +1,9 @@
 from pg_chameleon import *
 my_db=my_db_connection('config/my_connection.conf')
+l_args=[
+        my_db.ob_engine,
+        my_db.ob_conn,
+        my_db.ob_metadata
+        ]
+my_ddl=my_data_def(l_args)
+my_ddl.build_tab_list()
