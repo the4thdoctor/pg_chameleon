30931ed91e229b33c41b1022fbd7f8456c3cf5c9
the4thdoctor <4thdoctor.gallifrey@gmail.com>
2013-08-12 21:25:30 +0100

    added the build table's DDL to the pg_data_def class in the pg_lib.py file

--- pg_chameleon/lib/pg_lib.py
+++ pg_chameleon/lib/pg_lib.py
@@ -9,8 +9,18 @@ class pg_data_def:
         """ the function iterates over the list l_tables and build a new list with the statements """
         for l_table in self.l_tables:
             print "building the dll for table "+l_table[0]
-            t_ddl="CREATE TABLE "+'"'+l_table[0]+'"'
+            t_head="CREATE TABLE "+'"'+l_table[0]+'" ('
+            t_tail=");"
+            l_body=[]
             l_columns=l_table[1]
             for l_column in l_columns:
-                t_col_name=l_column[0] 
-                print l_column[1] 
+                t_col_name='"'+l_column[0]+'"' 
+                t_col_type=l_column[1]
+                t_col_null=l_column[2]
+                t_col_sec=l_column[3]
+                if t_col_sec=='AUTOINC':
+                    t_col_type='serial'
+                
+                l_body.append(t_col_name+ ' ' + t_col_type +' '+ t_col_null)
+            t_body=str(',').join(l_body)
+            print t_head+t_body+t_tail
