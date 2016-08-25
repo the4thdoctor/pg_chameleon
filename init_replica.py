7f1350abd176d8a0d0b7db66797674d958931234
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2016-08-25 12:59:17 +0100

    pull data working

--- init_replica.py
+++ init_replica.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python
 from pg_chameleon import mysql_engine
 mysql_eng=mysql_engine()
+mysql_eng.pull_table_data(limit=30000)
--- pg_chameleon/lib/mysql_lib.py
+++ pg_chameleon/lib/mysql_lib.py
@@ -9,7 +9,7 @@ class mysql_connection:
 		self.my_database=self.global_conf.my_database
 		self.my_connection=None
 		self.my_cursor=None
-		self.tables=[]
+		
 	
 	def connect_db(self):
 		"""  Establish connection with the database """
@@ -25,12 +25,10 @@ class mysql_connection:
 		self.my_connection.close()
 		
 		
-class mysql_engine():
-	def __init__(self):
-		self.mysql_con=mysql_connection()
-		self.mysql_con.connect_db()
-		self.get_table_metadata()
-		self.my_tables={}
+class mysql_engine:
+	def __init__(self, out_dir="/tmp/"):
+		self.out_dir=out_dir
+		self.my_tables=[]
 		self.type_dictionary={
 												'integer':'integer',
 												'mediumint':'bigint',
@@ -49,6 +47,9 @@ class mysql_engine():
 												'longblob':'bytea',
 												'blob':'bytea'
 										}
+		self.mysql_con=mysql_connection()
+		self.mysql_con.connect_db()
+		self.get_table_metadata()
 
 	def get_column_metadata(self, table):
 		sql_columns="""SELECT 
@@ -92,7 +93,7 @@ class mysql_engine():
 		return index_data
 	
 	def get_table_metadata(self):
-		print "getting tables"
+		print "getting table metadata"
 		sql_tables="""SELECT 
 											table_schema,
 											table_name
@@ -107,8 +108,35 @@ class mysql_engine():
 		for table in table_list:
 			column_data=self.get_column_metadata(table["table_name"])
 			index_data=self.get_index_metadata(table["table_name"])
-			dic_table={table["table_name"]:{'columns':column_data,  'indices': index_data}}
-			print dic_table
+			dic_table={'name':table["table_name"], 'columns':column_data,  'indices': index_data}
+			self.my_tables.append(dic_table)
 	
+	def pull_table_data(self, table=None, limit=10000):
+		
+		for table in self.my_tables:
+			column_list=[]
+			table_name=table["name"]
+			table_columns=table["columns"]
+			sql_count="SELECT count(*) as i_cnt FROM `"+table_name+"` ;"
+			self.mysql_con.my_cursor.execute(sql_count)
+			count_rows=self.mysql_con.my_cursor.fetchone()
+			num_slices=count_rows["i_cnt"]/limit
+			range_slices=range(num_slices+1)
+			for column in table_columns:
+				column_list.append("COALESCE(REPLACE("+column["column_name"]+", '\"', '\"\"'),'NULL') ")
+			columns="REPLACE(CONCAT('\"',CONCAT_WS('\",\"',"+','.join(column_list)+"),'\"'),'\"NULL\"','NULL')"
+			out_file=self.out_dir+'/out_data'+table_name+'.csv'
+			csv_file=open(out_file, 'wb')
+			for slice in range_slices:
+				sql_out="SELECT "+columns+" as data FROM "+table_name+" LIMIT "+str(slice*limit)+", "+str(limit)+";"
+				print "pulling out "+str(slice) + " of "+str(num_slices)
+				self.mysql_con.my_cursor.execute(sql_out)
+				csv_results = self.mysql_con.my_cursor.fetchall()
+				for csv_row in csv_results:
+					csv_file.write(csv_row["data"]+"\n")
+				
+			csv_file.close()
+			
+			
 	def __del__(self):
 		self.mysql_con.disconnect_db()
