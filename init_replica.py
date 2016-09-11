9f7f42f5fb13e71880940fe7f889afabbe35b96e
Federico Campoli <rotellaro@gmail.com>
2016-09-11 13:45:54 +0100

    remove init_replica.py

--- init_replica.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from pg_chameleon import replica_engine
-replica=replica_engine()
-replica.create_service_schema(cleanup=True)
-replica.create_tables(drop_tables=True)
-replica.copy_table_data(table_limit=100000)
-replica.create_indices()
-replica.do_stream_data()
