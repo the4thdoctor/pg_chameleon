47a28abf2cd4b618e7f5a1853e34c8cdb226acda
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2016-09-17 22:32:44 +0200

    change theme to blue and add source code to the api docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath('../pg_chameleon/lib'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [ 'sphinx.ext.autodoc']
+extensions = [ 'sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -123,7 +123,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
