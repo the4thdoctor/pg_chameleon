976105eaf4ac935826a099c6334d959db4cdb55c
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2016-09-24 07:06:14 +0100

    changed tokenisation with regular expressions

--- pg_chameleon/lib/sql_util.py
+++ pg_chameleon/lib/sql_util.py
@@ -1,26 +1,15 @@
+import re
 class sql_utility:
 	def __init__(self):
 		self.stop_chars=['\n', ' ', '\t']
 		self.statements=[]
-		self.normalised_words=[]
+		self.tokens=[]
 		
-	def serialise_sql(self, sql_string):
-		stat_elements=[]
-		stat_word=[]
-
+	def tokenise(self, sql_string):
+		token_list=[]
+		
 		self.statements=sql_string.split(';')
 		for statement in self.statements:
-			statement=statement.strip()
-			for chr in statement:
-				if chr in self.stop_chars and len(stat_word)>0:
-					stat_elements.append(''.join(stat_word))
-					stat_word=[]
-				else:
-					stat_word.append(chr)
-				
-			if len(stat_word)>0:
-					stat_elements.append(''.join(stat_word))
-					stat_word=[]
-			self.normalised_words.append(stat_elements)
-			stat_elements=[]
-		print self.normalised_words
+			token_list=re.sub("[^\w][(][)]", " ",  statement).split()
+			self.tokens.append(token_list)
+		print self.tokens
