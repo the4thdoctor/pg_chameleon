34a8d6019a8b75cf968d8aa7d2492d188f12b9c2
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2016-10-06 07:56:33 +0100

    tokeninsation of create table complete and mostly working

--- parse.py
+++ parse.py
@@ -18,7 +18,7 @@ CREATE   TABLE `store` (
   UNIQUE KEY idx_unique_manager (manager_staff_id),
   KEY idx_fk_address_id (address_id),
   index
-  idx_fk_address_id (address_id),
+  idx_fk_address_id (address_id,store_id),
   CONSTRAINT fk_store_staff FOREIGN KEY (manager_staff_id) REFERENCES staff (staff_id) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT fk_store_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -27,4 +27,6 @@ CREATE   TABLE `store` (
 
 token_sql=sql_token()
 token_sql.parse_sql(statement)
+for token in token_sql.tokenised:
+	print token
 #print parsesql.query_list
--- pg_chameleon/lib/sql_util.py
+++ pg_chameleon/lib/sql_util.py
@@ -6,13 +6,13 @@ class sql_token:
 	Each statement is converted in a python dictionary being used by pg_engine.
 	"""
 	def __init__(self):
-		self.statements=[]
+		self.tokenised=[]
 		self.query_list=[]
 		#re for keys and indices
 		self.m_pkeys=re.compile(r',\s*PRIMARY\s*KEY\s*\((.*?)\)\s*', re.IGNORECASE)
-		self.m_ukeys=re.compile(r',\s*UNIQUE\s*KEY\s*\w*\s*\((.*?)\)\s*', re.IGNORECASE)
-		self.m_idx=re.compile(r',\s*(KEY|INDEX)\s*\w*\s*\((.*?)\)\s*', re.IGNORECASE)
-		self.m_fkeys=re.compile(r',\s*CONSTRAINT\s*\w*\s*FOREIGN\s*KEY.*,', re.IGNORECASE)
+		self.m_ukeys=re.compile(r',\s*UNIQUE\s*KEY\s*`?\w*`?\s*\((.*?)\)\s*', re.IGNORECASE)
+		self.m_idx=re.compile(r',\s*(?:KEY|INDEX)\s*`?\w*`?\s*\((.*?)\)\s*', re.IGNORECASE)
+		self.m_fkeys=re.compile(r',\s*CONSTRAINT\s*`?\w*`?\s*FOREIGN\s*KEY.*,', re.IGNORECASE)
 		
 		#re for column constraint and auto incremental
 		self.m_nulls=re.compile(r'(NOT)?\s*(NULL)', re.IGNORECASE)
@@ -36,27 +36,35 @@ class sql_token:
 			if autoinc:
 				col_dic["autoinc"]="true"
 		return col_dic
-		
 	
+	def build_key_dic(self, inner_stat):
+		key_dic={}
+		inner_stat=inner_stat.strip()
+		#print inner_stat
+		pkey=self.m_pkeys.findall(inner_stat)
+		ukey=self.m_ukeys.findall(inner_stat)
+		idx=self.m_idx.findall(inner_stat)
+		if pkey:
+			key_dic["pkey"]=pkey
+		if ukey:
+			key_dic["ukey"]=ukey
+		if idx:
+			key_dic["idx"]=idx
+		return key_dic
 	def build_column_dic(self, inner_stat):
 		cols_parse=[]
 		column_list=inner_stat.split(',')
 		for col_def in column_list:
 			col_def=col_def.strip()
-			pkey=self.m_pkeys.match(col_def)
-			ukey=self.m_ukeys.match(col_def)
-			fkey=self.m_fkeys.match(col_def)
-			idx=self.m_idx.match(col_def)
-			if pkey or ukey or idx or fkey:
-				pass 
-			else:
-				print "column definition: "+col_def
-				col_dic=self.parse_column(col_def)
-				if col_dic:
-					cols_parse.append(col_dic)
+			#print "column definition: "+col_def
+			col_dic=self.parse_column(col_def)
+			if col_dic:
+				cols_parse.append(col_dic)
 		return cols_parse
+		
 	
 	def parse_create_table(self, sql_create):
+		table_dic={}
 		cnt_open=0
 		cnt_close=0
 		inner_buffer=[]
@@ -75,10 +83,10 @@ class sql_token:
 		column_list=self.m_ukeys.sub( '', column_list)
 		column_list=self.m_idx.sub( '', column_list)
 		column_list=self.m_fkeys.sub( '', column_list)
-		
 		print column_list
-		return self.build_column_dic(column_list)
-			
+		table_dic["keys"]=self.build_key_dic(inner_stat)
+		table_dic["columns"]=self.build_column_dic(column_list)
+		return table_dic	
 		
 	def parse_sql(self, sql_string):
 		"""
@@ -88,8 +96,9 @@ class sql_token:
 			
 			:param sql_string: The sql string with the sql statements.
 		"""
-		self.statements=sql_string.split(';')
-		for statement in self.statements:
+		
+		statements=sql_string.split(';')
+		for statement in statements:
 			stat_dic={}
 			stat_cleanup=re.sub(r'/\*.*?\*/', '', statement, re.DOTALL)
 			stat_cleanup=re.sub(r'--.*?\n', '', stat_cleanup)
@@ -101,13 +110,13 @@ class sql_token:
 			stat_cleanup=stat_cleanup.strip()
 			#print stat_cleanup
 			mcreate_table=self.m_create_table.match(stat_cleanup)
-			print mcreate_table
+			#print mcreate_table
 			if mcreate_table:
 				
 				command=' '.join(mcreate_table.group(1).split()).upper().strip()
 				stat_dic["command"]=command
 				stat_dic["identifier"]=mcreate_table.group(2)
-				print command
+				#print command
 				if command=='CREATE TABLE':
-					stat_dic["columns"]=self.parse_create_table(stat_cleanup)
-					print stat_dic
+					stat_dic["definition"]=self.parse_create_table(stat_cleanup)
+					self.tokenised.append(stat_dic)
