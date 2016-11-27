86cc9ebc76f4494efb02db2cc5c99fd78855ee26
Federico Campoli <rotellaro@gmail.com>
2016-11-27 17:50:40 +0000

    setup for alpha1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='pg_chameleon',
-	version='0.1',
-	description='Migration and replica extension from mysql to postgresql',
+	version='1.0.alpha.1',
+	description='MySql to PostgreSQL replication system',
 	url='https://github.com/the4thdoctor/pg_chameleon',
 	author='Federico Campoli',
 	author_email='4thdoctor.gallifrey@gmail.com',
@@ -10,10 +10,10 @@ setup(name='pg_chameleon',
 	packages=['pg_chameleon'],
 	zip_safe=False,
 	install_requires=[
-								'PyMySQL==0.7.6', 
-								'argparse==1.2.1', 
-								'mysql-replication==0.9', 
-								'psycopg2==2.6.2', 
-								'wsgiref==0.1.2', 
-								'PyYAML==3.11'
+								'PyMySQL', 
+								'argparse', 
+								'mysql-replication', 
+								'psycopg2', 
+								'wsgiref', 
+								'PyYAML'
 	],)
