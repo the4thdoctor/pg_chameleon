84d7e124b0957f4d34b63a3648cc0956d48e084c
Federico Campoli <rotellaro@gmail.com>
2016-11-27 11:32:05 +0000

    setup for python packaging

--- pg_chameleon/__init__.py
+++ pg_chameleon/__init__.py
@@ -1,5 +1,5 @@
-from lib.sql_util import *
-from lib.mysql_lib import *
-from lib.pg_lib import *
-from lib.global_lib import *
+from .sql_util import *
+from .mysql_lib import *
+from .pg_lib import *
+from .global_lib import *
 
--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup
 
 setup(name='pg_chameleon',
-	version='1.0 ALPHA1',
+	version='1.a1',
 	description='MySql to PostgreSQL replication system',
 	url='https://github.com/the4thdoctor/pg_chameleon',
 	author='Federico Campoli',
 	author_email='4thdoctor.gallifrey@gmail.com',
 	license='BSD License',
 	packages=['pg_chameleon'],
-	zip_safe=False,
+	download_url = 'https://github.com/the4thdoctor/pg_chameleon/tarball/1.0_ALPHA1',
 	install_requires=[
 								'PyMySQL==0.7.6', 
 								'argparse==1.2.1', 
 								'mysql-replication==0.9', 
 								'psycopg2==2.6.2', 
-								'wsgiref==0.1.2', 
-								'PyYAML==3.11'
-	],)
+								'PyYAML==3.11',
+								'sphinx==1.4.6',
+								'sphinx-autobuild==0.6.0'
+
+							],
+	scripts=['pg_cham']
+	)
+
