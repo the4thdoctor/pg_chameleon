cf98f77c6ab777e1e441eeee4a15be5a96c9d7bc
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-02-26 09:27:55 +0000

    setup workin, issues with the script

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ The tool can pull the data from a cascading replica when the MySQL slave is conf
 	packages=['pg_chameleon'],
 	py_modules=[
 		"pg_chameleon.__init__",
-		"pg_chameleon.lib.__init__",
 		"pg_chameleon.lib.global_lib",
 		"pg_chameleon.lib.mysql_lib",
 		"pg_chameleon.lib.pg_lib",
