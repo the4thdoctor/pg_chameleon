fb0837765694c2dd2d9646bd755fa02d7fc45730
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-03-10 09:31:46 +0100

    add lineno to log line

--- pg_chameleon/lib/global_lib.py
+++ pg_chameleon/lib/global_lib.py
@@ -133,7 +133,7 @@ class replica_engine(object):
 		self.logger = logging.getLogger(__name__)
 		self.logger.setLevel(logging.DEBUG)
 		self.logger.propagate = False
-		formatter = logging.Formatter("%(asctime)s: [%(levelname)s] - %(filename)s: %(message)s", "%b %e %H:%M:%S")
+		formatter = logging.Formatter("%(asctime)s: [%(levelname)s] - %(filename)s (%(lineno)s): %(message)s", "%b %e %H:%M:%S")
 		
 		if self.global_config.log_dest=='stdout' or stdout:
 			fh=logging.StreamHandler(sys.stdout)
