a284dc4b081fbe8c434c6960d748a60935177339
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-07-01 08:08:57 +0100

    add fields to t_replica_tables and initial sql on scratch

--- sql/upgrade/cat_1.4.sql
+++ sql/upgrade/cat_1.4.sql
@@ -1,8 +1,6 @@
-ALTER TABLE sch_chameleon.t_sources ADD  v_log_table character varying[];
-UPDATE sch_chameleon.t_sources 
-	SET v_log_table=ARRAY[
-		format('t_log_replica_1_src_%s',i_id_source),
-		format('t_log_replica_2_src_%s',i_id_source)
-	]
-;
+ALTER TABLE sch_chameleon.t_replica_tables
+	ADD  
+		t_binlog_name text,
+		i_binlog_position integer
+  ;
 
