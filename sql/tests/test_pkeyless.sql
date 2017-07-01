479eefef2662f2146c6a977dbbee87210400918f
Federico Campoli <rotellaro@gmail.com>
2017-07-01 10:12:19 +0100

    setup migration, add test_pkeyless.sql to check the sync

---
+++ sql/tests/test_pkeyless.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS test_pkeyless;
+CREATE TABLE test_pkeyless (
+  id SMALLINT UNSIGNED NOT NULL,
+  value1 VARCHAR(45) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO test_pkeyless (id,value1)
+VALUES
+(1,'dave'),
+(2,'neo'),
+(3,'phoenix'),
+(4,'is 42');
+
+/*
+stop the replica then run  call prepare_data then run this additional inserts */
+
+
+INSERT INTO test_pkeyless (id,value1)
+VALUES
+(5,'rainbow'),
+(6,'twilight'),
+(7,'pinkie'),
+(8,'apple')
+(9,'rarity')
+;
+
--- sql/upgrade/cat_1.4.sql
+++ sql/upgrade/cat_1.4.sql
@@ -1,6 +1,5 @@
 ALTER TABLE sch_chameleon.t_replica_tables
-	ADD  
-		t_binlog_name text,
-		i_binlog_position integer
+	ADD  t_binlog_name text,
+	ADD	i_binlog_position integer
   ;
 
