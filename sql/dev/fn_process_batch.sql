1be43a4ebbca4af79475087c13454c60b3c6893e
Federico Campoli <rotellaro@gmail.com>
2017-07-31 18:01:51 +0100

    improve replay function with queue table which allows the replay loop to access the events using the log table's primary key

--- sql/dev/fn_process_batch.sql
+++ sql/dev/fn_process_batch.sql
@@ -1,3 +1,4 @@
+
 CREATE OR REPLACE FUNCTION sch_chameleon.fn_process_batch(integer,integer)
 RETURNS BOOLEAN AS
 $BODY$
@@ -11,6 +12,8 @@ $BODY$
 		v_i_replayed		integer;
 		v_i_skipped		integer;
 		v_i_ddl		integer;
+		v_i_evt_replay	bigint[];
+		v_i_evt_queue		bigint[];
 	BEGIN
 		v_b_loop:=FALSE;
 		v_i_replayed:=0;
@@ -32,6 +35,27 @@ $BODY$
 			LIMIT 1
 			)
 		;
+		
+		
+
+		v_i_evt_replay:=(
+			SELECT 
+				i_id_event[1:p_i_max_events] 
+			FROM 
+				sch_chameleon.t_batch_events 
+			WHERE 
+				i_id_batch=v_i_id_batch
+		);
+
+		v_i_evt_queue:=(
+			SELECT 
+				i_id_event[p_i_max_events+1:array_length(i_id_event,1)] 
+			FROM 
+				sch_chameleon.t_batch_events 
+			WHERE 
+				i_id_batch=v_i_id_batch
+		);
+
 		IF v_i_id_batch IS NULL 
 		THEN
 			RETURN v_b_loop;
@@ -78,46 +102,89 @@ $BODY$
 				enm_binlog_event
 			FROM
 			(
-				SELECT 
+				SELECT
 					i_id_event,
 					i_id_batch,
 					v_table_name,
 					v_schema_name,
 					enm_binlog_event,
+					t_query,
+					ts_event_datetime,
+					t_pk_data,
+					t_pk_update,
 					array_agg(quote_ident(t_column)) AS t_colunm,
-					array_agg(quote_literal(jsb_event_data->>t_column)) as t_event_data,
-					array_agg(jsb_event_update->>t_column) as t_event_update,
-					string_agg(distinct format('%I=%L',t_column,jsb_event_update->>t_column),',') as  t_update,
-					string_agg(distinct format('%I=%L',v_pkey,jsb_event_data->>v_pkey),' AND ') as  t_pk_data,
-					string_agg(distinct format('%I=%L',v_pkey,jsb_event_update->>v_pkey),' AND ') as  t_pk_update,
-					t_query
+					string_agg(distinct format('%I=%L',t_column,jsb_event_data->>t_column),',') as  t_update,
+					array_agg(quote_nullable(jsb_event_data->>t_column)) as t_event_data
 				FROM
 				(
-					
-					SELECT 
-						log.i_id_event,
-						log.i_id_batch,
-						log.v_table_name,
-						log.v_schema_name,
-						log.enm_binlog_event,
-						log.jsb_event_data,
-						log.jsb_event_update,
-						log.t_query,
-						replace(unnest(string_to_array(v_table_pkey[1],',')),'"','') as v_pkey,
+					SELECT
+						i_id_event,
+						i_id_batch,
+						v_table_name,
+						v_schema_name,
+						enm_binlog_event,
+						jsb_event_data,
+						jsb_event_update,
+						t_query,
 						ts_event_datetime,
-						(jsonb_each_text(coalesce(log.jsb_event_data,'{"foo":"bar"}'::jsonb))).key AS t_column
-						
-						
-					FROM 
-						sch_chameleon.t_log_replica  log
-						INNER JOIN sch_chameleon.t_replica_tables tab
-							ON
-									tab.v_table_name=log.v_table_name
-								AND tab.v_schema_name=log.v_schema_name
-					WHERE
-							log.i_id_batch=v_i_id_batch
-					
-				) t_dat
+						string_agg(distinct format('%I=%L',v_pkey,jsb_event_data->>v_pkey),' AND ') as  t_pk_data,
+						string_agg(distinct format('%I=%L',v_pkey,jsb_event_update->>v_pkey),' AND ') as  t_pk_update,
+						(jsonb_each_text(coalesce(jsb_event_data,'{"foo":"bar"}'::jsonb))).key AS t_column
+					FROM
+					(
+						SELECT 
+							i_id_event,
+							i_id_batch,
+							v_table_name,
+							v_schema_name,
+							enm_binlog_event,
+							jsb_event_data,
+							jsb_event_update,
+							t_query,
+							ts_event_datetime,
+							replace(unnest(string_to_array(v_table_pkey[1],',')),'"','') as v_pkey
+							
+							
+							
+						FROM 
+							(
+								SELECT 
+									log.i_id_event,
+									log.i_id_batch,
+									log.v_table_name,
+									log.v_schema_name,
+									log.enm_binlog_event,
+									log.jsb_event_data,
+									log.jsb_event_update,
+									log.t_query,
+									ts_event_datetime,
+									v_table_pkey
+									
+									
+									
+								FROM 
+									sch_chameleon.t_log_replica  log
+									INNER JOIN sch_chameleon.t_replica_tables tab
+										ON
+												tab.v_table_name=log.v_table_name
+											AND tab.v_schema_name=log.v_schema_name
+								WHERE
+										log.i_id_batch=v_i_id_batch
+									AND 	log.i_id_event=ANY(v_i_evt_replay) 
+							) t_log
+							
+					) t_pkey
+					GROUP BY
+						i_id_event,
+						i_id_batch,
+						v_table_name,
+						v_schema_name,
+						enm_binlog_event,
+						jsb_event_data,
+						jsb_event_update,
+						t_query,
+						ts_event_datetime
+				) t_columns
 				GROUP BY
 					i_id_event,
 					i_id_batch,
@@ -125,9 +192,10 @@ $BODY$
 					v_schema_name,
 					enm_binlog_event,
 					t_query,
-					ts_event_datetime
-				ORDER BY ts_event_datetime
-			) t_query
+					ts_event_datetime,
+					t_pk_data,
+					t_pk_update
+			) t_sql
 		LOOP 	
 			EXECUTE  v_r_rows.t_sql;
 			IF v_r_rows.enm_binlog_event='ddl'
@@ -138,10 +206,7 @@ $BODY$
 			END IF;
 			
 			
-			DELETE FROM sch_chameleon.t_log_replica
-			WHERE
-				i_id_event=v_r_rows.i_id_event
-			;
+			
 			
 		END LOOP;
 		
@@ -165,7 +230,10 @@ $BODY$
 				i_id_batch=v_i_id_batch
 			;
 
-			
+			DELETE FROM sch_chameleon.t_batch_events
+			WHERE
+				i_id_batch=v_i_id_batch
+			;
 
 			v_b_loop=False;
 		ELSE
@@ -177,9 +245,28 @@ $BODY$
 			WHERE
 				i_id_batch=v_r_rows.i_id_batch
 			;
+
+			UPDATE sch_chameleon.t_batch_events
+				SET
+					i_id_event = v_i_evt_queue
+			WHERE
+				i_id_batch=v_i_id_batch
+			;
+
+			DELETE FROM sch_chameleon.t_log_replica
+			WHERE
+					i_id_batch=v_i_id_batch
+				AND 	i_id_event=ANY(v_i_evt_replay) 
+			;
+			
 			v_b_loop=True;
+
+
+			
 		END IF;
 
+		
+		
 		RETURN v_b_loop;
 
 	
