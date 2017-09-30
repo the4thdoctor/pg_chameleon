3adb5924efebcc35073604281d597f0db0f7efbc
Federico Campoli <rotellaro@gmail.com>
2017-09-30 09:52:00 +0100

    add source and schema arguments. add help for arguments

--- scripts/chameleon.py
+++ scripts/chameleon.py
@@ -19,11 +19,15 @@ commands = [
 	]
 
 command_help = """Available commands, """ + ','.join(commands)
-config_help = """Specifies the configuration to use. If  the parameter is omitted then it defaults to ~/.pg_chameleon/configuration/default.yml"""
+config_help = """Specifies the configuration to use without the suffix yml. If  the parameter is omitted then ~/.pg_chameleon/configuration/default.yml is used"""
+schema_help = """Specifies the schema within a source. If omitted all schemas for the given source are affected by the command. Requires the argument --source to be specified"""
+source_help = """Specifies the source withing a configuration. If omitted all sources are affected by the command."""
 
 parser = argparse.ArgumentParser(description='Command line for pg_chameleon.',  add_help=True)
 parser.add_argument('command', type=str, help=command_help)
 parser.add_argument('--config', type=str,  default='default',  required=False, help=config_help)
+parser.add_argument('--schema', type=str,  default='*',  required=False, help=schema_help)
+parser.add_argument('--source', type=str,  default='*',  required=False, help=source_help)
 
 args = parser.parse_args()
 
