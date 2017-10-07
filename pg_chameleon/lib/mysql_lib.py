771edc0fe62f1f740b951744cad037ed3a37d48d
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-10-08 00:03:38 +0200

    filtering the table list using  table_limit and table_skip is working

--- pg_chameleon/lib/mysql_lib.py
+++ pg_chameleon/lib/mysql_lib.py
@@ -73,34 +73,32 @@ class mysql_source(object):
 		"""
 			The method builds two dictionaries from the limit_tables and skip tables values set for the source.
 			The dictionaries are intended to be used in the get_table_list to cleanup the list of tables per schema.
-			
-			
 		"""
 		self.limit_tables = {}
 		self.skip_tables = {}
 		limit_tables = self.source_config["limit_tables"]
 		skip_tables = self.source_config["skip_tables"]
-		table_limit = [table.split('.') for table in limit_tables]
-		table_skip = [table.split('.') for table in skip_tables]
-		for table_list in table_limit:
-			list_exclude = []
-			try:
-				list_exclude = self.limit_tables[table_list[0]] 
-				list_exclude.append(table_list[1])
-			except KeyError:
-				list_exclude.append(table_list[1])
-			self.limit_tables[table_list[0]]  = list_exclude
+		if limit_tables:
+			table_limit = [table.split('.') for table in limit_tables]
+			for table_list in table_limit:
+				list_exclude = []
+				try:
+					list_exclude = self.limit_tables[table_list[0]] 
+					list_exclude.append(table_list[1])
+				except KeyError:
+					list_exclude.append(table_list[1])
+				self.limit_tables[table_list[0]]  = list_exclude
+		if skip_tables:
+			table_skip = [table.split('.') for table in skip_tables]		
+			for table_list in table_skip:
+				list_exclude = []
+				try:
+					list_exclude = self.skip_tables[table_list[0]] 
+					list_exclude.append(table_list[1])
+				except KeyError:
+					list_exclude.append(table_list[1])
+				self.skip_tables[table_list[0]]  = list_exclude
 				
-		for table_list in table_skip:
-			list_exclude = []
-			try:
-				list_exclude = self.skip_tables[table_list[0]] 
-				list_exclude.append(table_list[1])
-			except KeyError:
-				list_exclude.append(table_list[1])
-			self.skip_tables[table_list[0]]  = list_exclude
-				
-		print (self.limit_tables)
 
 	def get_table_list(self):
 		"""
@@ -120,9 +118,21 @@ class mysql_source(object):
 		for schema in self.schema_list:
 			self.cursor_buffered.execute(sql_tables, (schema))
 			table_list = [table["table_name"] for table in self.cursor_buffered.fetchall()]
+			try:
+				limit_tables = self.limit_tables[schema]
+				if len(limit_tables) > 0:
+					table_list = [table for table in table_list if table in limit_tables]
+			except KeyError:
+				pass
+			try:
+				skip_tables = self.skip_tables[schema]
+				if len(skip_tables) > 0:
+					table_list = [table for table in table_list if table not in skip_tables]
+			except KeyError:
+				pass
+			
 			self.schema_tables[schema] = table_list
-		
-		
+	
 		
 	def init_replica(self):
 		"""
@@ -135,7 +145,6 @@ class mysql_source(object):
 		self.schema_list = self.pg_engine.get_schema_list()
 		self.build_table_exceptions()
 		self.get_table_list()
-
-
+		
 
 
