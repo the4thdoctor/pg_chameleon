4f4dc3a3c3fc7fb48a7b0a92c2c98940d1742fdd
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-11-19 08:57:24 +0000

    add travisCI integration

--- tests/test.sh
+++ tests/test.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e 
 here=`dirname $0`
-chameleon.py set_config
-cp ${here}/test.yaml ~/.pg_chameleon/config/ 
-chameleon.py create_schema --config test
-chameleon.py add_source --config test
-chameleon.py init_replica --config test
-chameleon.py start_replica --config test &
-chameleon.py show_status --config test
-chameleon.py stop_replica --config test
-chameleon.py drop_schema --config test
+chameleon.py set_configuration_files
+cp ${here}/test.yml ~/.pg_chameleon/configuration/ 
+chameleon.py create_replica_schema --config test
+chameleon.py add_source --config test --source mysql
+chameleon.py init_replica --config test --source mysql --debug
+chameleon.py start_replica --config test --source mysql
+chameleon.py show_status --config test --source mysql
+chameleon.py stop_replica --config test --source mysql
+chameleon.py drop_replica_schema --config test
 
 
