b3ddbce4235aa741964e04cde962e22c9f6bb4ad
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2017-12-03 08:04:54 +0000

    update tests for pgsql source type

--- tests/test.sh
+++ tests/test.sh
@@ -5,7 +5,9 @@ chameleon.py set_configuration_files
 cp ${here}/test.yml ~/.pg_chameleon/configuration/ 
 chameleon.py create_replica_schema --config test
 chameleon.py add_source --config test --source mysql
+chameleon.py add_source --config test --source pgsql
 chameleon.py init_replica --config test --source mysql --debug
+chameleon.py init_replica --config test --source pgsql --debug
 chameleon.py start_replica --config test --source mysql
 chameleon.py show_status --config test --source mysql
 chameleon.py stop_replica --config test --source mysql
