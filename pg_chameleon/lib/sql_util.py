the4thdoctor/pg_chameleon

commit 8c09ccb5528c65c4eac347ad69f15b1b805f5bb1

Fix read replica crash if in alter table a column was defined as character varying

pg_chameleon/lib/sql_util.py
@@ -56,7 +56,7 @@ class sql_token(object):
 		self.m_alter_index = re.compile(r'(?:(ALTER\s+?TABLE)\s+(`?\b.*?\b`?))\s+((?:ADD|DROP)\s+(?:UNIQUE)?\s*?(?:INDEX).*,?)', re.IGNORECASE)
 		self.m_alter_table = re.compile(r'(?:(ALTER\s+?TABLE)\s+(`?\b.*?\b`?))\s+((?:ADD|DROP|CHANGE|MODIFY)\s+(?:\bCOLUMN\b)?.*,?)', re.IGNORECASE)
 		self.m_alter_list = re.compile(r'((?:\b(?:ADD|DROP|CHANGE|MODIFY)\b\s+(?:\bCOLUMN\b)?))(.*?,)', re.IGNORECASE)
-		self.m_alter_column = re.compile(r'\(?\s*`?(\w*)`?\s*(\w*)\s*(?:\((.*?)\))?\)?', re.IGNORECASE)
+		self.m_alter_column = re.compile(r'\(?\s*`?(\w*)`?\s*(\w*(?:\s*\w*)?)\s*(?:\((.*?)\))?\)?', re.IGNORECASE)
 		self.m_default_value = re.compile(r"(\bDEFAULT\b)\s*('?\w*'?)\s*", re.IGNORECASE)
 		self.m_alter_change = re.compile(r'\s*`?(\w*)`?\s*`?(\w*)`?\s*(\w*)\s*(?:\((.*?)\))?', re.IGNORECASE)
 		self.m_drop_primary = re.compile(r'(?:(?:ALTER\s+?TABLE)\s+(`?\b.*?\b`?)\s+(DROP\s+PRIMARY\s+KEY))', re.IGNORECASE)
@@ -349,7 +349,7 @@ class sql_token(object):
 					if alter_column:
 						alter_dic["command"] = command
 						alter_dic["name"] = alter_column.group(1).strip().strip('`')
-						alter_dic["type"] = alter_column.group(2).lower()
+						alter_dic["type"] = alter_column.group(2).lower().strip()
 						try:
 							alter_dic["dimension"]=alter_column.group(3).replace('|', ',').strip()
 						except:
@@ -379,7 +379,7 @@ class sql_token(object):
 					if alter_column:
 						alter_dic["command"] = command
 						alter_dic["name"] = alter_column.group(1).strip().strip('`')
-						alter_dic["type"] = alter_column.group(2).lower()
+						alter_dic["type"] = alter_column.group(2).lower().strip()
 						try:
 							alter_dic["dimension"]=alter_column.group(3).replace('|', ',').strip()
 						except:

parse.py
@@ -86,6 +86,7 @@ ADD COLUMN newstatus INT(10) UNSIGNED NOT NULL AFTER `log`;
 ALTER TABLE `test` DROP PRIMARY KEY;
 
 				"""
+statement="""alter table test_tiny add column value3 character varying(30);"""
 
 
 token_sql=sql_token()

commit 9a3dbbcdfb5f02d407845a68deac6e97258cd3a6

fix crash introduced in commit 8c09ccb.  when ALTER TABLE ADD COLUMN is in the form datatype DEFAULT (NOT) NULL the parser captures two words instead of one

pg_chameleon/lib/sql_util.py
@@ -344,6 +344,9 @@ class sql_token(object):
 					alter_dic["name"] = alter_item[1].strip().strip(',').replace('`', '').strip()
 				elif command == 'ADD':
 					alter_string = alter_item[1].strip()
+					alter_string = re.sub(r'DEFAULT', '', alter_string, re.IGNORECASE)
+					alter_string = re.sub(r'NOT', '', alter_string, re.IGNORECASE)
+					alter_string = re.sub(r'NULL', '', alter_string, re.IGNORECASE)
 					alter_column=self.m_alter_column.search(alter_string)
 					default_value = self.m_default_value.search(alter_string)
 					if alter_column:
@@ -375,7 +378,11 @@ class sql_token(object):
 							alter_dic["dimension"]=0
 					
 				elif command == 'MODIFY':
-					alter_column=self.m_alter_column.search(alter_item[1].strip())
+					alter_string = self.m_alter_column.search(alter_item[1].strip())
+					alter_string = re.sub(r'DEFAULT', '', alter_string, re.IGNORECASE)
+					alter_string = re.sub(r'NOT', '', alter_string, re.IGNORECASE)
+					alter_column = re.sub(r'NULL', '', alter_string, re.IGNORECASE)
+					
 					if alter_column:
 						alter_dic["command"] = command
 						alter_dic["name"] = alter_column.group(1).strip().strip('`')