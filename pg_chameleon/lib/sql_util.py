7698d2ab9516199be5c3f1efd5ae7d097ab9d122
Federico Campoli <4thdoctor.gallifrey@gmail.com>
2018-06-23 17:29:14 +0200

    simplify the regexp for match alter table

--- pg_chameleon/lib/sql_util.py
+++ pg_chameleon/lib/sql_util.py
@@ -54,9 +54,9 @@ class sql_token(object):
 		self.m_drop_table = re.compile(r'(DROP\s*TABLE)\s*(?:IF\s*EXISTS)?\s*(?:`)?(\w*)(?:`)?', re.IGNORECASE)
 		self.m_truncate_table = re.compile(r'(TRUNCATE)\s*(?:TABLE)?\s*(?:(?:`)?(\w*)(?:`)?)(?:.)?(?:`)?(\w*)(?:`)?', re.IGNORECASE)
 		self.m_alter_index = re.compile(r'(?:(ALTER\s+?TABLE)\s+(`?\b.*?\b`?))\s+((?:ADD|DROP)\s+(?:UNIQUE)?\s*?(?:INDEX).*,?)', re.IGNORECASE)
-		self.m_alter_table = re.compile(r'(?:(ALTER\s+?TABLE)\s+(`?\b.*?\b`?))\s+((?:ADD|DROP|CHANGE|MODIFY)\s+(?:\bCOLUMN\b)?.*,?)', re.IGNORECASE)
+		#self.m_alter_table = re.compile(r'(?:(ALTER\s+?TABLE)\s+(`?\b.*?\b`?))\s+((?:ADD|DROP|CHANGE|MODIFY)\s+(?:\bCOLUMN\b)?.*,?)', re.IGNORECASE)
 		self.m_alter_list = re.compile(r'((?:\b(?:ADD|DROP|CHANGE|MODIFY)\b\s+(?:\bCOLUMN\b)?))(.*?,)', re.IGNORECASE)
-		self.m_add_group = re.compile(r'(?:\bADD\b(\bCOLUMN\b)?)\s+(\(.+\))', re.IGNORECASE)
+		self.m_alter_table = re.compile(r'(\bALTER\b\s+\bTABLE\b)\s+(.*?)\s+(.*)', re.IGNORECASE)
 		self.m_alter_column = re.compile(r'\(?\s*`?(\w*)`?\s*(\w*(?:\s*\w*)?)\s*(?:\((.*?)\))?\)?', re.IGNORECASE)
 		self.m_default_value = re.compile(r"(\bDEFAULT\b)\s*('?\w*'?)\s*", re.IGNORECASE)
 		self.m_alter_change = re.compile(r'\s*`?(\w*)`?\s*`?(\w*)`?\s*(\w*)\s*(?:\((.*?)\))?', re.IGNORECASE)
@@ -344,8 +344,6 @@ class sql_token(object):
 					alter_dic["command"] = command
 					alter_dic["name"] = alter_item[1].strip().strip(',').replace('`', '').strip()
 				elif command == 'ADD':
-					print(alter_stat)
-					print(self.m_add_group.search(alter_stat))
 					alter_string = alter_item[1].strip()
 					
 					alter_column=self.m_alter_column.search(alter_string)
