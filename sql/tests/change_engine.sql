24a9a4f85cbcbc1ee26118df38bd27892511aa3c
Federico Razzoli <federico.razzoli.dba@gmail.com>
2018-06-28 18:43:10 +0100

    Add test for ALTER TABLE ENGINE = ...

---
+++ sql/tests/change_engine.sql
@@ -0,0 +1,8 @@
+-- MySQL supports different storage engines, PostgreSQL doesn't.
+-- The ALTER TABLE to change a table engine should be ignore.
+
+CREATE TABLE test_engine (
+    id INT PRIMARY KEY
+) ENGINE MEMORY;
+INSERT INTO test_engine (id) VALUES (100);
+ALTER TABLE test_engine ENGINE InnoDB;
