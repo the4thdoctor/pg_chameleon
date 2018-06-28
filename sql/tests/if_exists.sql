d417c058c9940eca5ebcda32d33199a672d5237b
Federico Razzoli <federico.razzoli@mbpc15-g0x7htdd.tw.com>
2018-06-28 09:37:48 +0100

    Add test for DROP TABLE IF EXISTS

---
+++ sql/tests/if_exists.sql
@@ -0,0 +1,23 @@
+-- DROP TABLE IF EXISTS is supported by both MySQL and PostgreSQL
+-- but we want to check if it works when a table only exists in MySQL,
+-- or it only works in PostgreSQL.
+-- Note that MySQL always replicates IF EXISTS statements.
+
+-- Create table in MySQL, then drop it in PostgreSQL
+-- and test DROP IF EXISTS
+SET SESSION sql_log_bin = 0;
+CREATE TABLE exists_mysql (
+    id INT PRIMARY KEY
+);
+SET SESSION sql_log_bin = 1;
+DROP TABLE IF EXISTS exists_mysql;
+
+-- Create the table in both DBMSs, then drop it in MySQL
+-- and test DROP IF EXISTS
+CREATE TABLE exists_postgresql (
+    id INT PRIMARY KEY
+);
+SET SESSION sql_log_bin = 0;
+DROP TABLE exists_postgresql;
+SET SESSION sql_log_bin = 1;
+DROP TABLE IF EXISTS exists_postgresql;
