a5a2c3b2bfe09d1246492da700b9a2b056b1dbb0
Kaustubh M <37668193+nikochiko@users.noreply.github.com>
2023-06-03 08:01:05 +0000

    Set python version to 3.7 and above

--- setup.py
+++ setup.py
@@ -33,8 +33,7 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Database :: Database Engines/Servers",
         "Topic :: Other/Nonlisted Topic"
     ],
@@ -62,7 +61,7 @@ setup(
     include_package_data = True,
     package_data=package_data,
     packages=setuptools.find_packages(),
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     keywords='postgresql mysql replica migration database',
 
 )
