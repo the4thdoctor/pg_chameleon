dae9fc66f585c534a0647714716edf64d1958088
Kaustubh M <37668193+nikochiko@users.noreply.github.com>
2023-07-03 16:52:08 +0000

    Port test_tokenizer from pytest to unittest
    Changes:
    - Port test_tokenizer from pytest to unittest
    - Make it an executable

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,5 +1,6 @@
-import pytest
+#!/usr/bin/env python
 
+import unittest
 
 from pg_chameleon.lib.sql_util import sql_token
 
@@ -7,62 +8,61 @@ from pg_chameleon.lib.sql_util import sql_token
 tokenizer = sql_token()
 
 
-@pytest.mark.parametrize(
-    "parser, string, expectation",
-    [
+class TestTokenizer(unittest.TestCase):
+    param_list = [
         (
-            tokenizer.column_definition,
+            "column_definition",
             "id int PRIMARY KEY",
             dict(column_name="id", data_type="int",
                  dimensions=None, enum_list=None, extras=["PRIMARY KEY"])
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "CONSTRAINT pk_id PRIMARY KEY (id)",
             dict(index_name="PRIMARY", index_columns=["id"], non_unique=0)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "CONSTRAINT pk_id1_id2 PRIMARY KEY (id1,id2)",
             dict(index_name="PRIMARY", index_columns=["id1", "id2"], non_unique=0)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "PRIMARY\n KEY(id1, id2, `id3`, `id 4` )",
             dict(index_name="PRIMARY", index_columns=["id1", "id2", "id3", "id 4"], non_unique=0)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "UNIQUE KEY (id1, id2)",
             dict(index_name="UNIQUE", index_columns=["id1", "id2"], non_unique=0)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "UNIQUE KEY (id1)",
             dict(index_name="UNIQUE", index_columns=["id1"], non_unique=0)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "UNIQUE (id1, id2)",
             dict(index_name="UNIQUE", index_columns=["id1", "id2"], non_unique=0)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "KEY (id1)",
             dict(index_name="INDEX", index_columns=["id1"], non_unique=1)
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "FULLTEXT KEY asdf (id)",
             dict(index_name="OTHER", index_columns=["id"], non_unique=1),
         ),
         (
-            tokenizer.any_key_definition,
+            "any_key_definition",
             "FOREIGN KEY (column1) REFERENCES table2 (column2)",
             dict(index_name="FOREIGN", index_columns=["column1"], non_unique=1)
         ),
         (
-            tokenizer.column_definition,
+            "column_definition",
             """film_rating smallint not null
             DEFAULT 3""",
             dict(column_name="film_rating", data_type="smallint",
@@ -70,7 +70,7 @@ tokenizer = sql_token()
                  extras=["NOT NULL", ["DEFAULT", "3"]])
         ),
         (
-            tokenizer.column_definition,
+            "column_definition",
             """film_rating
             ENUM ('1 star', '2 star', '3 star', '4 star', '5 star')
             DEFAULT '3 star'""",
@@ -79,7 +79,16 @@ tokenizer = sql_token()
                  extras=[["DEFAULT", "'3 star'"]])
         )
     ]
-)
-def test_parses_successfully(parser, string, expectation):
-    output = parser.parse(string)
-    assert output == expectation
+
+    def test_parses_successfully(self):
+        for parser_name, string, expected in self.param_list:
+            parser = getattr(tokenizer, parser_name)
+            with self.subTest(
+                msg=f"Checking parser {parser_name}", string=string, expected=expected
+            ):
+                output = parser.parse(string)
+                self.assertEqual(output, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
