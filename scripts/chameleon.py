805c445a1b906f8402739320f7864d28cf53d75a
Federico Campoli <thedoctor@pgdba.org>
2026-04-24 14:32:55 +0000

    start work on bringing compatibility with python 3.12.+

--- scripts/chameleon.py
+++ scripts/chameleon.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-from pkg_resources import get_distribution
-__version__ = get_distribution('pg_chameleon')
+try:
+    from pkg_resources import get_distribution
+    __version__ = get_distribution('pg_chameleon')
+except:
+    from importlib.metadata import version
+    __version__=version('pg_chameleon')
 import argparse
 from pg_chameleon import replica_engine
 
